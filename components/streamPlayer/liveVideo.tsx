0cb705a66356e02917bab69744da094a9b0c39df
agusgrance <agustingran@gmail.com>
2024-06-24 16:35:09 -0300

    add fix to LiveVideo component

--- components/streamPlayer/liveVideo.tsx
+++ components/streamPlayer/liveVideo.tsx
@@ -1,11 +1,7 @@
 "use client";
 import React, { useRef, useState, useEffect } from "react";
-
-import { useEventListener } from "usehooks-ts";
 import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
-import FullScreenControl from "./fullscreenControl";
-import VolumeControl from "./volumeControl";
 
 interface Props {
   participant: Participant;
@@ -14,7 +10,6 @@ const LiveVideo = ({ participant }: Props) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
-  const [isFullScreen, setIsFullScreen] = useState(false);
   const [volume, setVolume] = useState(0);
 
   const onVolumeChange = (value: number) => {
@@ -39,21 +34,6 @@ const LiveVideo = ({ participant }: Props) => {
     onVolumeChange(0);
   }, []);
 
-  const toggleFullScreen = () => {
-    if (isFullScreen) {
-      document.exitFullscreen();
-    } else if (wrapperRef?.current) {
-      wrapperRef.current.requestFullscreen();
-    }
-  };
-
-  const handleFullScreenChange = () => {
-    const isCurrentlyFullScreen = document.fullscreenElement !== null;
-    setIsFullScreen(isCurrentlyFullScreen);
-  };
-
-  useEventListener("fullscreenchange", handleFullScreenChange, wrapperRef);
-
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
     .forEach((track) => {
@@ -64,20 +44,7 @@ const LiveVideo = ({ participant }: Props) => {
 
   return (
     <div ref={wrapperRef} className="relative h-full flex">
-      <video ref={videoRef} width="100%" />
-      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
-        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
-          <VolumeControl
-            onChange={onVolumeChange}
-            onToggle={toggleMute}
-            value={volume}
-          />
-          <FullScreenControl
-            isFullScreen={isFullScreen}
-            onToggle={toggleFullScreen}
-          />
-        </div>
-      </div>
+      <video controls ref={videoRef} width="100%" />
     </div>
   );
 };
