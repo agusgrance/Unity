57dabf2c627b17d9ff79a55dd24103e84cc6900d
agusgrance <agustingran@gmail.com>
2024-06-24 15:27:03 -0300

    add Responsive logo

--- components/browse/logo/index.tsx
+++ components/browse/logo/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import LogoImg from "@/assets/img/logo.png";
+import LogoIcon from "@/assets/img/logoIcon.png";
 
 const Logo = () => {
   return (
     <Link href="/">
       <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
         <Image width={100} alt="Logo" src={LogoImg} />
+      </div>
+      <div className="flex lg:hidden items-center gap-x-4 hover:opacity-75 transition mr-4">
+        <Image width={48} alt="Logo" src={LogoIcon} />
       </div>
     </Link>
   );
--- components/ui/thumbnail.tsx
+++ components/ui/thumbnail.tsx
@@ -37,7 +37,7 @@ const Thumbnail = ({ src, fallback, isLive, username }: Props) => {
   }
   return (
     <div className="group aspect-video relative rounded-md cursor-pointer">
-      <div className="rounded-md absolute inset-0 bg-blue-600 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center" />
+      <div className="rounded-md absolute inset-0 bg-[#ff4057] opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center" />
       {content}
       {isLive && src && (
         <div className="absolute top-2 left-2 transition-transform group-hover:translate-x-2 group-hover:-translate-y-2">
