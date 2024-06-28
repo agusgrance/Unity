12402a61721f50edd0da0b1d01414841ab5ab605
agusgrance <agustingran@gmail.com>
2024-06-27 21:35:27 -0300

    add error log

--- app/error.tsx
+++ app/error.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
-const ErrorPage = () => {
+interface ErrorPageProps {
+  error: Error;
+}
+
+const ErrorPage: React.FC<ErrorPageProps> = ({ error }) => {
+  useEffect(() => {
+    if (error) {
+      console.error("Error details:", error);
+    }
+  }, [error]);
+
   return (
     <div className="h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground">
       <p>Something went wrong</p>
-
       <Button variant="secondary" asChild>
         <Link href="/">Go back home</Link>
       </Button>
